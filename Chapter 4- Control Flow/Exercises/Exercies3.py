1b791da50b37d5d4379d5babffed44e6ce837f87
AlexaMeg01 <alexa.mabagbsu@gmail.com>
2023-11-02 10:07:50 +0400

    Exercise3

---
+++ Chapter 4- Control Flow/Exercises/Exercies3.py
@@ -0,0 +1,8 @@
+alien_color = 'blue'
+
+if alien_color == 'red':
+    print("You just earned 5 points!")
+elif alien_color == 'blue':
+    print("You just earned 10 points!")
+else:
+    print("You just earned 20 points!")
