598beffeec8786ff6269f2537aae7a65791db26a
AlexaMeg01 <alexa.mabagbsu@gmail.com>
2023-11-02 10:09:26 +0400

    Exercise2

---
+++ Chapter 5- Dictionaries/Exercises/Exercies2.py
@@ -0,0 +1,21 @@
+favorite_numbers = {
+    'Erica': 42,
+    'Elena': 23,
+    'Shuna': 7,
+    'Sherle': 1000000,
+    'Lusia': 0,}
+
+num = favorite_numbers['Erica']
+print("Erica's favorite number is " + str(num) + ".")
+
+num = favorite_numbers['Elena']
+print("Elena's favorite number is " + str(num) + ".")
+
+num = favorite_numbers['Shuna']
+print("Shuna's favorite number is " + str(num) + ".")
+
+num = favorite_numbers['Sherle']
+print("Sherle's favorite number is " + str(num) + ".")
+
+num = favorite_numbers['Lusia']
+print("Lusia's favorite number is " + str(num) + ".")
