6e4142196f731f1fa449e0ad549d0945f0ec17df
AlexaMeg01 <alexa.mabagbsu@gmail.com>
2023-11-02 10:09:42 +0400

    Exercise3

---
+++ Chapter 5- Dictionaries/Exercises/Exercies3.py
@@ -0,0 +1,17 @@
+glossary = {
+    'Anime': 'Reborn as a Vending Machine, I Now Wander the Dungeon',
+    'comment': 'the story is really great',
+    'Type': 'TV',
+    'Genres': 'Adventure, Comedy, Fantasy, Parody, Isekai, Magic',
+    'Episodes': "12" }
+
+word = 'Anime'
+print("\n" + word.title() + ": " + glossary[word])
+word = 'comment'
+print("\n" + word.title() + ": " + glossary[word])
+word = 'Type'
+print("\n" + word.title() + ": " + glossary[word])
+word = 'Genres'
+print("\n" + word.title() + ": " + glossary[word])
+word = 'Episodes'
+print("\n" + word.title() + ": " + glossary[word]) 
