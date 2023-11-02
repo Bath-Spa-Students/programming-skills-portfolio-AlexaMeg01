aee28c226259f02fded0d5ed9ba7ab498486c4fa
AlexaMeg01 <alexa.mabagbsu@gmail.com>
2023-11-02 10:05:42 +0400

    Exercies2

--- Chapter 2- Variables & Comments/Exercises/Exercies2.py
+++ Chapter 2- Variables & Comments/Exercises/Exercies2.py
@@ -1,2 +1,3 @@
-print ('''John Quincy Adams once said 
-"If your actions inspire others to dream more, learn more, do more and become more, you are a leader."''')
+print ('''John Quincy Adams once said''')
+print ('''"If your actions inspire others to dream more, learn more,
+        do more and become more, you are a leader."''')
