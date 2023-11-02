ddc19ede39068cdec4378ed81d6fe02070bd5976
AlexaMeg01 <alexa.mabagbsu@gmail.com>
2023-11-02 10:06:41 +0400

    Exercise5

--- Chapter 2- Variables & Comments/Exercises/Exercies5 .py
+++ Chapter 2- Variables & Comments/Exercises/Exercies5 .py
@@ -1,4 +1,7 @@
-pi=22/7
-degree = float(input("Input degrees: "))
-radian = degree*(pi/180)
-print(radian)
+r = float(input ("Amount of money : "))
+print ("Amount of money: " + str(r) + " Change: " + str(r-(6*8)) + " amout lost : " + str(6%8))
+
+usb_bought="USB Bought is ="
+amount_left="Total Amount that is left ="
+print (usb_bought + str(int(50/6)))
+print (amount_left + str(50%6))
