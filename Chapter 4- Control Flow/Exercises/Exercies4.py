5b29a33c947635cfcfff5538b314883eacf91a98
AlexaMeg01 <alexa.mabagbsu@gmail.com>
2023-11-02 10:08:19 +0400

    Exercise4

--- Chapter 4- Control Flow/Exercises/Exercies4.py
+++ Chapter 4- Control Flow/Exercises/Exercies4.py
@@ -1,5 +1,14 @@
-print ('''List of people to invite:''')
-print ('''Loreen
-Bea B.
-Drew
-Julie''')
+age = 18
+
+if age < 2:
+    print("You're a baby!")
+elif age < 4:
+    print("You're a toddler!")
+elif age < 18:
+    print("You're a kid!")
+elif age < 30:
+    print("You're a teenager!")
+elif age < 65:
+    print("You're an adult!")
+else:
+    print("You're an elder!")
