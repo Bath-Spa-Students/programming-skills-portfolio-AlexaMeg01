b9b707a5d95e73d44e1d1d18f81efaff62e39150
AlexaMeg01 <alexa.mabagbsu@gmail.com>
2023-11-07 13:37:18 +0400

    Exercise3

---
+++ Chapter 6- Loops/Exercises/Ch6_Exercise3.py
@@ -0,0 +1,11 @@
+while True:
+    age = input("How old are you? ")
+    
+    age = int(age)
+    if age < 3:
+        print("Your ticket is $10")
+    elif age < 12:
+        print("Your ticket is $20") 
+    else:
+        print("Your ticket is $25")
+        
